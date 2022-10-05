4dbb97b2209dc0564c25ca4d180dfdf8131e2d97
root <root@HOME-PC.localdomain>
2022-10-05 23:52:43 +0300

    many thing done

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,3 @@
+class BookController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root 'home#index'
+
+  get '/books', to: 'book#index'
 end
