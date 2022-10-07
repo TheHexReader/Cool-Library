ca9e8e7e24e61e3ab48d03a2b86c6b95dccf1fdc
TheHexReader <thehexreader@gmail.com>
2022-10-07 21:23:24 +0300

    fixed helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,7 +4,7 @@
 module HomeHelper
   def get_author_full_name(id)
     author = Author.find_by(id:)
-    "#{author.middle_name} #{author.last_name} #{author.last_name}"
+    "#{author.middle_name} #{author.first_name} #{author.last_name}"
   end
 
   def get_status_name(id)
