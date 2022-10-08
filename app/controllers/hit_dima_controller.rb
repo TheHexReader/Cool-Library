5c73808d988b96c3bc319278d2fab24f75e27523
Hex <thehexreader@gmail.com>
2022-10-08 10:28:30 +0300

    fixed some things

--- app/controllers/hit_dima_controller.rb
+++ app/controllers/hit_dima_controller.rb
@@ -3,4 +3,5 @@
 # HitDima controller
 class HitDimaController < ApplicationController
   def index; end
+  def punch; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root       'home#index'
-  
+  root 'home#index'
+
   devise_for :users
 
-  resource   :books, only: %w[ show ]
-  
-  get        '/hit_dima', to: 'hit_dima#index'
+  resource :books, only: %w[show]
 
-  %w[ 404 422 500 503 ].each do |code|
-    get code, :to => "errors#show", :code => code
+  get '/hit_dima', to: 'hit_dima#index'
+  get '/hit_dima/punch', to: 'hit_dima#punch'
+
+  %w[404 422 500 503].each do |code|
+    get code, to: 'errors#show', code:
   end
 end
